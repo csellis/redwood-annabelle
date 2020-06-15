8f225f65cea3ea3a4c31c49720b85209878cbf9e
Chris Ellis <ellis.cs@gmail.com>
2020-06-15 07:37:52 -0400

    Save before rescaffold for relation

--- web/src/components/Prize/Prize.js
+++ web/src/components/Prize/Prize.js
@@ -27,57 +27,41 @@ const Prize = ({ prize }) => {
     }
   }
 
+  console.log(prize)
+
   return (
     <>
       <div className="rw-segment">
         <header className="rw-segment-header">
           <h2 className="rw-heading rw-heading-secondary">
             Prize {prize.id}
-            Detail{' '}
-          </h2>{' '}
-        </header>{' '}
+            Detail
+          </h2>
+        </header>
         <table className="rw-table">
           <tbody>
             <tr>
-              <th> id </th> <td> {prize.id} </td>{' '}
-            </tr>{' '}
+              <th> id </th> <td> {prize.id} </td>
+            </tr>
             <tr>
-              <th> name </th> <td> {prize.name} </td>{' '}
-            </tr>{' '}
+              <th> name </th> <td> {prize.name} </td>
+            </tr>
             <tr>
-              <th> imageUrl </th> <td> {prize.imageUrl} </td>{' '}
-            </tr>{' '}
+              <th> imageUrl </th> <td> {prize.imageUrl} </td>
+            </tr>
             <tr>
-              <th> lessonsNeeded </th> <td> {prize.lessonsNeeded} </td>{' '}
-            </tr>{' '}
+              <th> lessonsNeeded </th> <td> {prize.lessonsNeeded} </td>
+            </tr>
             <tr>
-              <th> lessonsCompleted </th> <td> {prize.lessonsCompleted} </td>{' '}
-            </tr>{' '}
+              <th> lessonsCompleted </th> <td> {prize.lessonsCompleted} </td>
+            </tr>
             <tr>
-              <th> createdAt </th> <td> {prize.createdAt} </td>{' '}
-            </tr>{' '}
-          </tbody>{' '}
-        </table>{' '}
-      </div>{' '}
-      <nav className="rw-button-group">
-        <Link
-          to={routes.editPrize({
-            id: prize.id,
-          })}
-          className="rw-button rw-button-blue"
-        >
-          Edit{' '}
-        </Link>{' '}
-        <a
-          href="#"
-          className="rw-button rw-button-red"
-          onClick={() => onDeleteClick(prize.id)}
-        >
-          Delete{' '}
-        </a>{' '}
-      </nav>
-      <hr className="border border-red-600" />
-      <div className="grid gap-4 grid-cols-2">
+              <th> createdAt </th> <td> {prize.createdAt} </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="grid gap-4 grid-cols-2 pt-8">
         <div className="rounded-lg shadow-lg overflow-hidden bg-gray-50 p-8">
           <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
             {prize.name}
@@ -94,6 +78,23 @@ const Prize = ({ prize }) => {
           />
         </div>
       </div>
+      <nav className="rw-button-group">
+        <Link
+          to={routes.editPrize({
+            id: prize.id,
+          })}
+          className="rw-button rw-button-blue"
+        >
+          Edit
+        </Link>
+        <a
+          href="#"
+          className="rw-button rw-button-red"
+          onClick={() => onDeleteClick(prize.id)}
+        >
+          Delete
+        </a>
+      </nav>
     </>
   )
 }
